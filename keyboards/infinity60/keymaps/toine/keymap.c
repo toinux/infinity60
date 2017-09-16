782728bd2b9a26717db434f1d8ef03af0d688c14
Martin, Antoine <antoine.martin@saint-gobain.com>
2017-09-16 13:29:17 +0200

    ajout raccourcis changement bureau

--- keyboards/infinity60/keymaps/toine/keymap.c
+++ keyboards/infinity60/keymaps/toine/keymap.c
@@ -76,7 +76,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |PgU|Up |PgD|Psc|Slk|Pus|     |
+     * |Caps |lg1|lg2|lg3|lg4|   |   |PgU|Up |PgD|Psc|Slk|Pus|     |
      * |-----------------------------------------------------------|
      * |Ctrl  |VoD|VoU|Mut|   |   |Hom|Lef|Dow|Rig|   |   |Enter   |
      * |-----------------------------------------------------------|
@@ -87,7 +87,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FN] = KEYMAP(
       ______, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL, \
-      KC_CAPS,_no___,_no___  ,_no___,_no___,_no___,_no___,KC_PGUP, KC_UP,KC_PGDN, KC_PSCR,KC_SLCK,KC_PAUS, ______, \
+      KC_CAPS,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),_no___,_no___,KC_PGUP, KC_UP,KC_PGDN, KC_PSCR,KC_SLCK,KC_PAUS, ______, \
       KC_LCTL,KC_VOLD,KC_VOLU,KC_MUTE,_no___,_no___,KC_HOME,KC_LEFT,KC_DOWN,KC_RGHT,_no___,_no___,______, \
       ______,_no___ ,_no___ ,_no___,_no___,KC_SPC,KC_END ,LCTL(LALT(KC_LEFT)),LCTL(LALT(KC_RIGHT)),_no___, MO(_SP),______,______, \
       ______ ,______,______,          ______,               ______,______,______,______),

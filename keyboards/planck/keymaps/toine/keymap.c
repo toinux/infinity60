3f4523b0a0ea43321a7c1ebb82e35553f4385f3d
toinux <toinux@gmail.com>
2018-01-20 16:22:48 +0100

    ajout de print screen + commentaires

--- keyboards/planck/keymaps/toine/keymap.c
+++ keyboards/planck/keymaps/toine/keymap.c
@@ -186,18 +186,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |Pause | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |Pause |Reset | Debug|RgbTog|RgbMod|RgbHui|RgbHud|RgbSai|RgbSad|RgbVai|RgbVad|  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |Gaming|
+ * |PrtScr|TGNKRO|MusMod|Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |Gaming|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
   {KC_PAUS, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD,  KC_DEL },
-  {_______,TG_NKRO,  MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______,  _______, GAMING },
+  {KC_PSCR,TG_NKRO,  MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______,  _______, GAMING },
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______,  _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______}
 }

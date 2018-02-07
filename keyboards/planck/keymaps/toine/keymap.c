36c5b838102820864b3e92c3896018d96404c16e
toinux <toinux@gmail.com>
2018-02-07 14:04:46 +0100

    inverse touches souris/accents

--- keyboards/planck/keymaps/toine/keymap.c
+++ keyboards/planck/keymaps/toine/keymap.c
@@ -21,7 +21,7 @@ extern keymap_config_t keymap_config;
 
 #define SPACEFN LT(_FN,KC_SPC)
 #define ESCFN LT(_FN,KC_ESC)
-#define ACSCLN LT(_ACCENTS,KC_SCLN)
+#define MOSCLN LT(_MOUSE,KC_SCLN)
 #define ACCENTS MO(_ACCENTS)
 #define SFTQUOT MT(MOD_RSFT,KC_QUOT)
 #define NUMPAD MO(_NUMPAD)
@@ -65,18 +65,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |Acct/;|Enter |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |Mous/;|Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |SftCap|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/' |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Esc/FN|NUMPAD| GUI  | Alt  |Lower | Space/FN    |Raise |MOUSE |AltGr |TG(FN)|MO(FN)|
+ * |Esc/FN|NUMPAD| GUI  | Alt  |Lower | Space/FN    |Raise |ACCENT|AltGr |TG(FN)|MO(FN)|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB     , KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_LCTL    , KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ACSCLN,  KC_ENT },
+  {KC_LCTL    , KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    MOSCLN,  KC_ENT },
   {TD(SFT_CAP), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFTQUOT},
-  {ESCFN      , NUMPAD, KC_LGUI, KC_LALT, LOWER,   SPACEFN, SPACEFN,  RAISE,  MOUSE,   KC_RALT, TG(_FN),  MO(_FN)}
+  {ESCFN      , NUMPAD, KC_LGUI, KC_LALT, LOWER,   SPACEFN, SPACEFN,  RAISE,  ACCENTS,   KC_RALT, TG(_FN),  MO(_FN)}
 },
 
 /* Gaming
